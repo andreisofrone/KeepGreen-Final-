cc2056a51b8d0f67993e29a0d001e15eb108e3b7
walcoot <andreisofronee@gmail.com>
2017-10-14 11:41:41 +0300

    Test

--- FieldInspection/UI/MainApplication.cs
+++ FieldInspection/UI/MainApplication.cs
@@ -20,7 +20,6 @@ namespace FieldInspection
         {
             base.OnCreate();
             RegisterActivityLifecycleCallbacks(this);
-            //A great place to initialize Xamarin.Insights and Dependency Services!
         }
 
         public override void OnTerminate()
